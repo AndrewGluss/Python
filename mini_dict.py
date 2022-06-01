b9fb3b55f3f1eae10a4875b486c138c45e13449d
AndrewGluss <bangluss@gmail.com>
2022-06-01 13:03:25 +0300

    show definition of term using dict data type

--- mini_dict.py
+++ mini_dict.py
@@ -4,4 +4,11 @@ list1 = []
 for i in range(n):
     #print(i)
     list1.append(input().split(": "))
-print(list1)
+m = int(input())
+list2 = [input() for i in range(int(input()))]
+#print(list2)
+for i in range(len(list_1)):
+	mini_dict[list_1[i][0].lower()] = list_1[i][1]
+#print(mini_dict)
+for i in list_2:
+    print(mini_dict.get(i.lower(), "Не найдено"))
