78a58e741ba091cd749dc7cadcaae0292b4c43c0
AndrewGluss <bangluss@gmail.com>
2022-07-26 16:07:07 +0300

    example of using zip()

---
+++ zip_func.py
@@ -0,0 +1,6 @@
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+
+for country, capital, popul in zip(countries,capitals,population):
+  print(capital,'is the capital of', country+ ', population equal', popul, 'people.')
