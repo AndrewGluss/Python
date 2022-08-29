c0ec2da9d4824b086d636f9d08922e164a369678
AndrewGluss <bangluss@gmail.com>
2022-08-29 16:40:09 +0300

    fix code and add using datetime module

--- validate_time.py
+++ validate_time.py
@@ -1,3 +1,19 @@
+import datetime
+
+def good_period(pice):
+    """
+    Функция проверяет на корректность временного отрезка
+    и возвращает True, когда отрезок корректный и False, когда некорректный.
+    """
+    try:
+        time1 = datetime.datetime.strptime(pice[0], '%H:%M:%S')
+        time2 = datetime.datetime.strptime(pice[1], '%H:%M:%S')
+        if time1 <= time2:
+            return True
+        return False
+    except ValueError:
+        return False
+
 def not_cross(list_period):
     """
     Функция проверяет на пересечение периодов
@@ -7,45 +23,32 @@ def not_cross(list_period):
     cross_list = []
     for i in range(len(list_period)):
         for j in range(i+1, len(list_period)):
-            if (list_period[i][0] == list_period[j][1]) or (
-                good_period([list_period[j][1],list_period[i][0]])==False) or (
-                good_period([list_period[j][0],list_period[i][1]])==False) or (
-                list_period[i][1] == list_period[j][0]
-            ):
-                cross_list.append(False)
+            if good_period(list_period[i]) and good_period(list_period[j]):
+                time1 = datetime.datetime.strptime(list_period[i][0], '%H:%M:%S')
+                time2 = datetime.datetime.strptime(list_period[i][1], '%H:%M:%S')
+                time3 = datetime.datetime.strptime(list_period[j][0], '%H:%M:%S')
+                time4 = datetime.datetime.strptime(list_period[j][1], '%H:%M:%S')
+                if time2 < time3 or time4 < time1:
+                    cross_list.append(True)
+                else:
+                    cross_list.append(False)
+                    break
             else:
-                cross_list.append(True)
+                cross_list.append(False)
+                break
     return all(cross_list)
 
-def good_period(pice):
-    """
-    Функция проверяет на корректность временного отрезка
-    и возвращает True, когда отрезок корректный и False, когда некорректный.
-    """
-    pices = []
-    for i in pice:
-        times = i.split(':')
-        pices.append(times)
-    if (int(pices[0][0])<=int(pices[1][0]) and (0<=int(pices[0][0])<=23 and 0<=int(pices[1][0])<=23)) and (
-        int(pices[0][1]) <= int(pices[1][1]) and (0 <= int(pices[0][1]) <= 59 and 0 <= int(pices[1][1]) <= 59)) and (
-        int(pices[0][2]) <= int(pices[1][2]) and (0 <= int(pices[0][2]) <= 59 and 0 <= int(pices[1][2]) <= 59)):
-        return True
-    else:
-        return False
 
 t = int(input())
 for i in range(t):
     n = int(input())
     count = 0
-    list_period = [] # список, понадобится для проверки пересечения
+    list_period = [0]*n # список, понадобится для проверки пересечения
     for j in range(n):
-        # period = [[k for k in j.split(':')] for j in input().split('-')]
-        # if int(period[0][0])<=int(period[0][0]) and int(period[0][1])<=int(period[0][1]) and int(period[0][2])<=int(period[0][2]):
         period = [k for k in input().split('-')]
         if good_period(period): # если промежуток времени правильный, увеличиваем счетчик на единицу
             count += 1
-        list_period.append(period)
-    # print(list_period)
+        list_period[j] = period
     if count == n and not_cross(list_period):  # если счетчик равен количеству промежутков и пересечений нет
         print("YES")
     else:
