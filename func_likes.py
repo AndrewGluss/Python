ac2107a4fc02dda19419ba7041f3cccaa1d53fb1
AndrewGluss <bangluss@gmail.com>
2022-09-02 12:18:50 +0300

    add another method to solve

--- func_likes.py
+++ func_likes.py
@@ -34,3 +34,13 @@ def likes(names):
         return names[0]+", "+  names[1] + " и "+ str(len(names)-2) +' других оценили данную запись'
     else:
         return 'Никто не оценил данную запись'
+
+# def likes(names):
+#     n = len(names)
+#     return {
+#         0: 'no one likes this',
+#         1: '{} likes this', 
+#         2: '{} and {} like this', 
+#         3: '{}, {} and {} like this', 
+#        4: '{}, {} and {others} others like this'
+#     }[min(4, n)].format(*names[:3], others=n-2)
